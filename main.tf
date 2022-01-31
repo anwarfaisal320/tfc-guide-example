9b79fba00696a122ba405f94613f0fb1d6145103
anwarfaisal320 <47579817+anwarfaisal320@users.noreply.github.com>
2022-01-31 15:16:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  root_block_device.encrypted = true
 
   tags = {
     Name                 = var.instance_name
